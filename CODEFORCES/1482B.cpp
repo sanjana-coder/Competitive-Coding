f5ef693c949ed34be894d940a6b1ae86cd0116cc
Sanjana <thanshan14@gmail.com>
2021-02-06 23:41:48 +0530

    DAY 20

--- CODEFORCES/1482B.cpp
+++ CODEFORCES/1482B.cpp
@@ -10,52 +10,16 @@ using namespace std;
 #define w(t) while(t--)
 int main()
 {
-    ll t;
-    cin>>t;
-    w(t)
-    {
-        ll n,k,p=0,i=0,o=0,max=0;
-        cin>>n>>k;
-        ll a[n];
-        rep(i,n)
-        {
-            cin>>a[i];
-            if(i>0)
-            {
-                if(a[i]>max) max=a[i]; o=i;
-            }
-        }
-        if(o==1) cout<<-1<<endl;
-        else if(n==1) cout<<-1<<endl;
-        else
-        {
-        while(k>0)
-        {
-            for(ll i=1;i<n;i++)
-            {
-                cout<<"i"<<i;
-                if(a[i]>a[i-1])
-                {
-                k = k-(a[i]-a[i-1]);
-                p=i-1;
-                cout<<"k"<<k;
-                cout<<"p"<<p;
-                break;
-                } 
-            
-            }
-            
-            
-        }
-            if(p==n-1) cout<<-1<<endl;
-            else
-            {
-                if(k<=0)
-                {
-                    cout<<p+1<<endl;
-                }
-            } 
-        }
-       
-    }
+    int t,n,k,h[110],p,ans;
+	cin>>t;
+	w(t) {
+		cin>>n>>k;
+		for (int i=1;i<=n;i++) cin>>h[i];
+		for (p=1;p<=k;p++) {
+			ans=-1;
+			for (int i=1;i<n;i++) if (h[i]<h[i+1]) {ans=i;h[i]++;break;}
+			if (ans==-1) break;
+		}
+		cout<<ans<<endl;
+	}
 }
